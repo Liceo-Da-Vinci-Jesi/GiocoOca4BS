b5c226893626bd1ef61b8ee9de9b81f24a67440d
AlessandroZingaretti <s7096111@liceodavincijesi.edu.it>
2022-04-28 15:11:00 +0200

    Update Lobby

--- src/Lobby.py
+++ src/Lobby.py
@@ -3,9 +3,9 @@ from PIL import Image
 
 class Lobby(wx.Frame):
     def __init__(self,listaIconeDisponibili):
-        super().__init__(None, title="Lobby")
-        self.listaIcone = listaIconeDisponibili
+        super().__init__(None, title="Giochi dei Paesaggi di Giacomo")
         self.giocatori = []
+        self.listaIcone = listaIconeDisponibili
         panel = wx.Panel(self)
         box = wx.BoxSizer(wx.VERTICAL)
         vbox = wx.BoxSizer(wx.VERTICAL)
@@ -15,62 +15,36 @@ class Lobby(wx.Frame):
         font13Norm = wx.Font(13, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
         font9Norm = wx.Font(13, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
         font25Bold = wx.Font(20, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-        ToggleButton1=wx.ToggleButton(panel, label="Giocatore 1",id = 1)
-        ToggleButton1.Bind(wx.EVT_TOGGLEBUTTON,self.sbloccaStato)
-        ToggleButton1.SetFont(font13Norm)
-        grid.Add(ToggleButton1,proportion = 0, flag=wx.ALL | wx.EXPAND, border=5)
-        
-        ToggleButton2=wx.ToggleButton(panel, label="Giocatore 2",id = 2)
-        ToggleButton2.Bind(wx.EVT_TOGGLEBUTTON, self.sbloccaStato)
-        ToggleButton2.SetFont(font13Norm)
-        grid.Add(ToggleButton2,proportion = 0, flag=wx.ALL | wx.EXPAND, border=5)
-        
-        ToggleButton3=wx.ToggleButton(panel, label="Giocatore 3",id = 3)
-        ToggleButton3.Bind(wx.EVT_TOGGLEBUTTON, self.sbloccaStato)
-        ToggleButton3.SetFont(font13Norm)
-        grid.Add(ToggleButton3,proportion = 0, flag=wx.ALL | wx.EXPAND, border=5)
-        
-        ToggleButton4=wx.ToggleButton(panel, label="Giocatore 4",id = 4)
-        ToggleButton4.Bind(wx.EVT_TOGGLEBUTTON, self.sbloccaStato)
-        ToggleButton4.SetFont(font13Norm)
-        grid.Add(ToggleButton4,proportion = 0, flag=wx.ALL | wx.EXPAND, border=5)
-        
-        textCtrl1 = wx.TextCtrl(panel,style = wx.TE_CENTRE|wx.TEXT_ALIGNMENT_CENTER)
-        textCtrl1.SetFont(font9Norm)
-        grid.Add(textCtrl1, proportion=0, flag =wx.ALL|wx.EXPAND, border=5)
-       
-        textCtrl2 = wx.TextCtrl(panel,style = wx.TE_CENTRE|wx.TEXT_ALIGNMENT_CENTER)
-        textCtrl2.SetFont(font9Norm)
-        grid.Add(textCtrl2, proportion=0, flag =wx.ALL|wx.EXPAND, border=5)
-        
-        textCtrl3 = wx.TextCtrl(panel,style = wx.TE_CENTRE|wx.TEXT_ALIGNMENT_CENTER)
-        textCtrl3.SetFont(font9Norm)
-        grid.Add(textCtrl3, proportion=0, flag =wx.ALL|wx.EXPAND, border=5)
-        
-        textCtrl4 = wx.TextCtrl(panel,style = wx.TE_CENTRE|wx.TEXT_ALIGNMENT_CENTER)
-        textCtrl4.SetFont(font9Norm)
-        grid.Add(textCtrl4, proportion=0, flag =wx.ALL|wx.EXPAND, border=5)
 
+        self.listaToggleButton = []
+        self.listaTc = []
+        self.listaViewer = []
+
+        #creazione e gestione dei vari elementi:
+        #pulsanti(togglebutton)
+        for n in range(1,5):
+            ToggleButton=wx.ToggleButton(panel, label="Giocatore "+str(n),id = n)
+            ToggleButton.Bind(wx.EVT_TOGGLEBUTTON,self.sbloccaStato)
+            ToggleButton.SetFont(font13Norm)
+            grid.Add(ToggleButton,proportion = 0, flag=wx.ALL | wx.EXPAND, border=5)
+            self.listaToggleButton.append(ToggleButton)
+
+        #textctrl
+        for n in range(4):
+            textCtrl = wx.TextCtrl(panel,style = wx.TE_CENTRE|wx.TEXT_ALIGNMENT_CENTER)
+            textCtrl.SetFont(font9Norm)
+            textCtrl.Disable()
+            grid.Add(textCtrl, proportion=0, flag =wx.ALL|wx.EXPAND, border=5)
+            self.listaTc.append(textCtrl)
+
+        #icone
         hbox=wx.BoxSizer(wx.HORIZONTAL)
-        bitmap1 = wx.Bitmap(self.listaIcone[0])
-        viewer1 = wx.StaticBitmap(panel,bitmap = bitmap1)
-        hbox.Add(viewer1,proportion = 1,flag = wx.ALL|wx.EXPAND,border = 5)
-
-        bitmap2 = wx.Bitmap(self.listaIcone[1])
-        viewer2 = wx.StaticBitmap(panel,bitmap = bitmap2)
-        hbox.Add(viewer2,proportion = 1,flag = wx.ALL|wx.EXPAND,border = 5)
-
-        bitmap3 = wx.Bitmap(self.listaIcone[2])
-        viewer3 = wx.StaticBitmap(panel,bitmap = bitmap3)
-        hbox.Add(viewer3,proportion = 1,flag = wx.ALL|wx.EXPAND,border = 5)
-
-        bitmap4 = wx.Bitmap(self.listaIcone[3])
-        viewer4 = wx.StaticBitmap(panel,bitmap = bitmap4)
-        hbox.Add(viewer4,proportion = 1,flag = wx.ALL|wx.EXPAND,border = 5)
-
-        self.listaToggleButton = (ToggleButton1,ToggleButton2,ToggleButton3,ToggleButton4)
-        self.listaTc = (textCtrl1,textCtrl2,textCtrl3,textCtrl4)
-        self.listaViewer = (viewer1,viewer2,viewer3,viewer4)
+        for n in self.listaIcone:
+            img = wx.Bitmap(n).ConvertToDisabled()
+            viewer = wx.StaticBitmap(panel,bitmap = wx.Bitmap(img))
+            viewer.SetSize((100,100))
+            self.listaViewer.append(viewer)
+            hbox.Add(viewer,proportion = 1,flag = wx.ALL|wx.EXPAND,border = 5)
 
         self.contaGiocatori = 0
         self.PIniziaPartita= wx.Button(panel, label="INIZIA PARTITA")
@@ -89,15 +63,6 @@ class Lobby(wx.Frame):
         self.SetIcon(wx.Icon("../icone/iconaInfinito.ico"))
         self.Refresh()
 
-        viewer1.SetBitmap(wx.Bitmap((100,100), depth=2))
-        textCtrl1.Disable()
-        viewer2.SetBitmap(wx.Bitmap((100,100), depth=2))
-        textCtrl2.Disable()
-        viewer3.SetBitmap(wx.Bitmap((100,100), depth=2))
-        textCtrl3.Disable()
-        viewer4.SetBitmap(wx.Bitmap((100,100), depth=2))
-        textCtrl4.Disable()
-    
     def sbloccaStato(self,event):
         self.Refresh()
         if self.listaToggleButton[event.GetId()-1].GetValue():
@@ -108,7 +73,8 @@ class Lobby(wx.Frame):
         else:
             self.contaGiocatori-=1
             self.listaTc[event.GetId()-1].Disable()
-            self.listaViewer[event.GetId()-1].SetBitmap(wx.Bitmap((100,100), depth=2))
+            img = wx.Bitmap(self.listaIcone[event.GetId()-1]).ConvertToDisabled()
+            self.listaViewer[event.GetId()-1].SetBitmap(wx.Bitmap(self.listaIcone[event.GetId()-1]).ConvertToDisabled())
         self.sbloccaTastoInizioPartita()
         self.Refresh()
         return
